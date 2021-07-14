e8cdb14157d35dc4dd64fbd7d3e97a718fcbbca5
Hongbi <dyongdi@gmail.com>
2021-07-14 16:01:34 +0900

    [#4] 효율성: FrogRiverOne

--- codility/frogRiverOne.js
+++ codility/frogRiverOne.js
@@ -1,12 +1,8 @@
 function solution(X, A) {
-  const arr = new Array(X + 1).fill(null);
-  arr[0] = 0;
-
-  for (let i = 0; i <= A.length; i++) {
-    arr[A[i]] = A[i];
-    if (arr.findIndex((el) => el === null) === -1) {
-      return i;
-    }
+  const set = new Set();
+  for (let i = 0; i < A.length; i++) {
+    set.add(A[i]);
+    if (set.size === X) return i;
   }
   return -1;
 }
