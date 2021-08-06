a0904468418176c4672c39d899450454994a1801
Hongbi <dyongdi@gmail.com>
2021-08-06 11:51:24 +0900

    [#9] 스택: Remove All Adjacent Duplicates in String II

--- leetcode/1209.RemoveAllAdjacentDuplicatesinStringII.js
+++ leetcode/1209.RemoveAllAdjacentDuplicatesinStringII.js
@@ -6,21 +6,20 @@
 var removeDuplicates = function (s, k) {
   // k개 연속이면 터뜨리기
   let stack = [];
+  const countStack = [];
 
   for (let el of s) {
-    // 새로운 el을 볼 때 직전 k-1개의 원소와 el이 같은지 확인
-    // 모두 같다면 stack에서 뒤에서 k-1개 pop (또는 slice(0, k))
-    // 하나라도 다르다면 그 다음 el로~
-    let flag = true;
-    for (let i = 1; i < k; i++) {
-      if (el === stack[stack.length - i]) continue;
+    if (el === stack[stack.length - 1]) {
+      countStack[countStack.length - 1]++;
+      if (countStack[countStack.length - 1] < k) stack.push(el);
       else {
-        flag = false;
-        stack.push(el);
-        break;
+        stack = stack.slice(0, -(k - 1));
+        countStack.pop();
       }
+    } else {
+      stack.push(el);
+      countStack.push(1);
     }
-    if (flag) stack = stack.slice(0, -(k - 1));
   }
 
   return stack.join('');
